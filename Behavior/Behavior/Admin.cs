9c6a333b5cb0406009ed73c4fa512d4d1e3520db
Justin Marshall <jlmarshall1@student.fullsail.edu>
2018-05-19 23:50:48 -0400

    AS

--- Behavior/Behavior/Admin.cs
+++ Behavior/Behavior/Admin.cs
@@ -13,7 +13,7 @@ namespace Behavior
 {
     public partial class Admin : Form
     {
-        SqlConnection cnn = new SqlConnection("Data Source=behaviordatabase.cfzuumxbilpn.us-east-2.rds.amazonaws.com," + "1433;Initial Catalog=Login;User ID=BehaviorTest;Password=jL55hrHErQMD");
+        SqlConnection cnn = new SqlConnection("Data Source=behaviordatabase.cfzuumxbilpn.us-east-2.rds.amazonaws.com," + "1433;Initial Catalog=Login;User ID=BehaviorTest;Password=jL55hrHErQMD;integrated security=False");
         public Admin()
         {
             InitializeComponent();
